3cdcafa10795aef6562f8dd5dedd795cab1c102c
daniyalha33 <daniyalha33@gmail.com>
2024-11-23 23:34:38 +0500

    database connection added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 const app=express()
 import dotenv from "dotenv"
+import connectDb from "./db/dbConnect.js";
 dotenv.config()
 const Port=process.env.PORT ||3000
-app.listen(Port,()=>console.log("Server is running on port 8000"))
-app.get("/",(req,res)=>[
+app.listen(Port,()=>{
+    connectDb();
+    console.log("Server is running on port 8000")})
+app.get("/",(req,res)=>{
+   
     console.log("server is running")
-])
+})
